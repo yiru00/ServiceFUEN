94ffac2251e7bf4d6baaf761740a2d4fc5adbf51
yiru00 <eva890706@gmail.com>
2023-02-15 01:53:15 +0800

    Activity Controller 1.寫好所需action 2.建ActivityVM、擴充方法 3.NewActivity()完成

--- ServiceFUEN/Controllers/ActivityController.cs
+++ ServiceFUEN/Controllers/ActivityController.cs
@@ -2,45 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServiceFUEN.Models.EFModels;
+using ServiceFUEN.Models.ViewModels;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ServiceFUEN.Controllers
 {
-    [Route("api/[controller]")]
+
+    [EnableCors("AllowAny")]
+    //[Route("api/[controller]")]
+   // [ApiController]
     public class ActivityController : Controller
     {
-        // GET: api/values
+        private readonly ProjectFUENContext _context;
+
+        public ActivityController(ProjectFUENContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Activity/NewActivity
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("api/Activity/NewActivity")]
+        public IEnumerable<ActivityVM> NewActivity()
         {
-            return new string[] { "value1", "value2" };
+            var projectFUENContext = _context.Activities.Include(a=>a.Category).Include(a => a.ActivityMembers).Include(a => a.ActivityCollections).OrderBy(a=>a.DateOfCreated).Select(a=>a.toActivityVM());
+
+            return projectFUENContext.ToList();
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET api/Activity/PopularActivity
+        [HttpGet]
+        [Route("api/Activity/PopularActivity")]
+        public ActivityVM PopularActivity()
         {
-            return "value";
+            
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
+        // GET api/Activity/Search
+        [HttpGet]
+        [Route("api/Activity/Search")]
+        public ActivityVM Search(string activity,string address,DateTime time)
         {
+            
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        // GET api/Activity/Details
+        [HttpGet]
+        [Route("api/Activity/Details")]
+        public ActivityCategoryVM Details(int activityId)
         {
+            
         }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        // GET api/Activity/Details
+        [HttpGet]
+        [Route("api/Activity/Category")]
+        public ActivityCategoryVM Category()
         {
+            
         }
     }
 }
