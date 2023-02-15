5cdc9d5ac2bd333abee4b5e2575932a3bb3e34dd
yiru00 <eva890706@gmail.com>
2023-02-15 15:22:21 +0800

    activity controller 1.Popular() 完成 2.Search() 完成 3.category() 完成    。建ActivityCategoryVM、toActivityCategoryVM擴充方法 4.WillBeHeld() 完成 5.Details() 完成   。建ActivityDetailsVM 、toActivityDetailsVM擴充方法 6.SameCategory() 完成

---
+++ ServiceFUEN/Models/Infrastructures/ExtensionMethods/ActivityCategoryExts.cs
@@ -0,0 +1,24 @@
+﻿using ServiceFUEN.Models.EFModels;
+
+namespace ServiceFUEN.Models.ViewModels
+{
+    public static partial class ActivityExts
+    {
+        public static ActivityVM toActivityVM(this Activity source)
+        {
+            return new ActivityVM
+            {
+                ActivityId = source.Id,
+                CoverImage = source.CoverImage,
+                ActivityName = source.ActivityName,
+                Address = source.Address,
+                CategoryName = source.Category.CategoryName,
+                GatheringTime = source.GatheringTime,
+                NumOfEnrolment = source.ActivityMembers.Count,
+                NumOfCollections = source.ActivityCollections.Count,
+                EnrolmentRate = source.ActivityMembers.Count / source.MemberLimit
+            };
+        }
+    }
+}
+
