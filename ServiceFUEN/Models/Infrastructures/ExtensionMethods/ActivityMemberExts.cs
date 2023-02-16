5da8291e9502116ec4b5b6b3f99db052736aeea9
yiru00 <eva890706@gmail.com>
2023-02-17 02:01:52 +0800

    建ActivityRecordController 1.Enrolled()、Joined() 完成   。建RecordReqDTO、RecordResVM   。建ToEnrollRecordResVM 擴充方法 2.Saved() 完成   。建EnrollRecordResVM   。建ToSaveRecordResVM 擴充方法

---
+++ ServiceFUEN/Models/Infrastructures/ExtensionMethods/ActivityMemberExts.cs
@@ -0,0 +1,30 @@
+﻿using ServiceFUEN.Models.EFModels;
+
+namespace ServiceFUEN.Models.ViewModels
+{
+    public static partial class EnrollRecordResVMExts
+    {
+        public static EnrollRecordResVM ToEnrollRecordResVM(this ActivityMember source)
+        {
+            return new EnrollRecordResVM
+            {
+                MemberId = source.MemberId,
+                ActivityId = source.ActivityId,
+                CoverImage = source.Activity.CoverImage,
+                ActivityName = source.Activity.ActivityName,
+                Recommendation = source.Activity.Recommendation,
+                Address = source.Activity.Address,
+                MemberLimit = source.Activity.MemberLimit,
+                Description = source.Activity.Description,
+                GatheringTime = source.Activity.GatheringTime,
+                Deadline = source.Activity.Deadline,
+                DateOfCreated = source.Activity.DateOfCreated,
+                EnrollId = source.Id,
+                DateJoined = source.DateJoined
+            };
+
+        }
+        
+    }
+}
+
