783af13b32c5d6af091c147d09b28f275a83d4af
yiru00 <eva890706@gmail.com>
2023-02-16 21:19:19 +0800

    ActivitySave controller 1.save()完成 2.unsave()完成 3.SaveStatus() 完成

---
+++ ServiceFUEN/Models/ViewModels/SaveResVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceFUEN.Models.ViewModels
+{
+    public class EnrollResVM
+    {
+
+        public bool result { get; set; }
+        public string message { get; set; }
+    }
+}
+
