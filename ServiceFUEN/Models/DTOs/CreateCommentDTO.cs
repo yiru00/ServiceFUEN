b273c3dc183dcf943ddc9d079992ad96099ac205
jim2764 <jim2345678@gmail.com>
2023-02-19 19:43:42 +0800

    CommentController
    Create
    Edit
    Delete

---
+++ ServiceFUEN/Models/DTOs/CreateCommentDTO.cs
@@ -0,0 +1,26 @@
+﻿using ServiceFUEN.Models.EFModels;
+
+namespace ServiceFUEN.Models.DTOs
+{
+	public class CreateCommentDTO
+	{
+		public int Id { get; set; }
+		public string Content { get; set; }
+		public int MemberId { get; set; }
+		public int PhotoId { get; set; }
+	}
+
+	public static partial class ExtensionMethods
+	{
+		public static Comment DtoToEntity(this CreateCommentDTO source)
+		{
+			return new Comment()
+			{
+				Id = source.Id,
+				Content = source.Content,
+				MemberId = source.MemberId,
+				PhotoId = source.PhotoId,
+			};
+		}
+	}
+}
