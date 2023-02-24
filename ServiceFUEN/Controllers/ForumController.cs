92630f144b1a1246f187ab7c254f03a52eabc2d3
shiun <shiunworks@gmail.com>
2023-02-24 20:52:58 +0800

    剩下文章edit

--- ServiceFUEN/Controllers/ForumController.cs
+++ ServiceFUEN/Controllers/ForumController.cs
@@ -21,9 +21,49 @@ namespace ServiceFUEN.Controllers
             _context = context;
         }
 
+        //顯示文章留言
+        [HttpGet]
+        [Route("api/Article/ArticleComment")]
+        public IEnumerable<MessageVM> ArticleComment(int messageId)
+        {
+            var projectFUENContext = _context.Messages
+                .Where(a => a.Id == messageId)
+                .Select(a => new MessageVM()
+                {
+                    Id= a.Id,
+                    Content = a.Content,
+                    Time = a.Time,
+                    MemberId = a.MemberId,
+                    ArticleId = a.Article.Id,
+                    NickName = a.Member.NickName,
+                });
+
+            return projectFUENContext;
+        }
+
+        //顯示選取的文章內容
+        [HttpGet]
+        [Route("api/Article/ArticleDetails")]
+        public IEnumerable<ArticleListVM> ArticleDetails(int ArticleId)
+        {
+            var projectFUENContext = _context.Articles
+                .Where(a => a.Id == ArticleId)
+                .Select(a => new ArticleListVM()
+                {
+                    ArticleId = a.Id,
+                    Title = a.Title,
+                    Time = a.Time,
+                    MemberId = a.MemberId,
+                    NickName = a.Member.NickName,
+                    ForumId = a.ForumId,
+                    ForumName = a.Forum.Name,
+                });
+
+            return projectFUENContext;
+        }
 
 
-        // 最新文章(依照新到舊時間)
+        // 全部最新文章(依照新到舊時間)
         [HttpGet]
         [Route("api/Article/LatestArticle")]
         public IEnumerable<ArticleListVM> LatestArticle()
@@ -44,7 +84,28 @@ namespace ServiceFUEN.Controllers
 
         }
 
-        // 最熱門文章(依照留言數)
+        //某看板最新文章(依照新到舊時間)
+        [HttpGet]
+        [Route("api/Article/ForumLatestArticle")]
+        public IEnumerable<ArticleListVM> ForumLatestArticle(int ForumId)
+        {
+            var projectFUENContext = _context.Articles
+               .Include(a => a.Member).Include(a => a.Forum).Include(a => a.Messages).Where(a => a.ForumId == ForumId).OrderByDescending(a => a.Time).Select(a => new ArticleListVM
+               {
+                   ArticleId = a.Id,
+                   MemberId = a.MemberId,
+                   Title = a.Title,
+                   ForumId = a.ForumId,
+                   Time = a.Time,
+                   NickName = a.Member.NickName,
+                   ForumName = a.Forum.Name,
+               })
+          .ToList();
+            return projectFUENContext;
+
+        }
+
+        // 全部最熱門文章(依照留言數)
         [HttpGet]
         [Route("api/Article/PopularArticle")]
         public IEnumerable<ArticleListVM> PopularArticle()
@@ -64,9 +125,29 @@ namespace ServiceFUEN.Controllers
             return projectFUENContext;
         }
 
-        // 找某分類的文章
+        //某看板最熱門文章(依照留言數)
         [HttpGet]
-        [Route("api/Article/PopularArticle")]
+        [Route("api/Article/ForumPopularArticle")]
+        public IEnumerable<ArticleListVM> ForumPopularArticle(int ForumId)
+        {
+            var projectFUENContext = _context.Articles
+                .Include(a => a.Member).Include(a => a.Forum).Include(a => a.Messages).Where(a => a.ForumId==ForumId).OrderByDescending(x => x.Messages.Count).Select(a => new ArticleListVM
+                {
+                    ArticleId = a.Id,
+                    MemberId = a.MemberId,
+                    Title = a.Title,
+                    ForumId = a.ForumId,
+                    Time = a.Time,
+                    NickName = a.Member.NickName,
+                    ForumName = a.Forum.Name,
+                })
+           .ToList();
+            return projectFUENContext;
+        }
+
+        // 找某看板的文章
+        [HttpGet]
+        [Route("api/Article/ForumArticle")]
         public IEnumerable<ArticleListVM> ForumArticle(int ForumId)
         {
             var projectFUENContext = _context.Articles
@@ -83,6 +164,7 @@ namespace ServiceFUEN.Controllers
            .ToList();
             return projectFUENContext;
         }
+        
         //Search文章標題
         [HttpGet]
         [Route("api/Article/Search")]
@@ -106,7 +188,6 @@ namespace ServiceFUEN.Controllers
             });
             return result;
         }
-
 
         //全部看板
         [HttpGet]
@@ -142,6 +223,7 @@ namespace ServiceFUEN.Controllers
 
 
         //新增留言(完成不確定)
+        //沒有驗證文章存不存在
         [HttpPost]
         [Route("api/Message/CreateComment")]
         public void CreateComment(MessageDto msg)
@@ -196,10 +278,73 @@ namespace ServiceFUEN.Controllers
             _context.SaveChanges();
         }
 
-        ////排序最新文章
-        //[HttpGet]
-        //[Route("api/Article/NewArticle")]
+       
 
 
+        //修改文章
+        [HttpPut]
+        [Route("api/Article/EditArticle/{id}")]
+        public void EditArticle(int id,[FromForm] CreateArticleVM articleVM)
+        {
+            List<ArticlePhoto> photos = new List<ArticlePhoto>();
+
+            foreach (var file in articleVM.Files)
+            {
+                // 移進資料夾
+                string path = System.Environment.CurrentDirectory + "/Images/";
+                string extension = Path.GetExtension(file.FileName);
+                string fileName = Guid.NewGuid().ToString("N");
+                string fullName = fileName + extension;
+                string fullPath = Path.Combine(path, fullName);
+                using (var stream = System.IO.File.Create(fullPath))
+                {
+                    file.CopyTo(stream);
+                }
+
+                // GUID檔名加進List
+                ArticlePhoto photo = new ArticlePhoto()
+                {
+                    Photo = fullName,
+                };
+                photos.Add(photo);
+            }
+
+            var article = _context.Articles.FirstOrDefault(a=>a.Id == id);
+            article.MemberId = articleVM.MemberId;
+            article.Title = articleVM.Title;
+            article.Content = articleVM.Content;
+            article.ArticlePhotos = photos;
+            article.ForumId = articleVM.ForumId;
+            //foreach(string item in articleVM.Photos)
+            //{
+
+            //    //article.ArticlePhotos.Add(photo); 一樣
+            //}
+            //article.ArticlePhotos = photos;
+
+            _context.Articles.Update(article);
+            _context.SaveChanges();
+        }
+        //刪除文章
+        [HttpDelete]
+        [Route("api/Article/DeleteArticle")]
+        public void DeleteArticle(int articleid)
+        {
+            var article = _context.Articles.FirstOrDefault(a => a.Id == articleid);
+            _context.Articles.Remove(article);
+            _context.SaveChanges();
+        }
+
+        //刪除文章留言
+        [HttpDelete]
+        [Route("api/Message/DeleteComment")]
+        public void DeleteComment(int messageid)
+        {
+            var message = _context.Messages.FirstOrDefault(a => a.Id == messageid);
+            _context.Messages.Remove(message);
+            _context.SaveChanges();
+        }
+
+        
     }
 }
---
+++ ServiceFUEN/Models/ViewModels/ArticleVM.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceFUEN.Models.ViewModels
+{
+    public class ArticleVM
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime Time { get; set; }
+        public int MemberId { get; set; }
+        public int ForumId { get; set; }
+
+    }
+}
