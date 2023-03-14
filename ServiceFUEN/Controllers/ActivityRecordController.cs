2b64079eb1682f40f9436b714daf795bc86a1ffe
DESKTOP-E1P73N6\kevin <kevin3571630@gmail.com>
2023-03-15 05:40:28 +0800

    我昨天生日耶

--- ServiceFUEN/Controllers/ActivityRecordController.cs
+++ ServiceFUEN/Controllers/ActivityRecordController.cs
@@ -13,7 +13,7 @@ using ServiceFUEN.Models.ViewModels;
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ServiceFUEN.Controllers
-{
+{ 
     [EnableCors("AllowAny")]
     [ApiController]
     public class ActivityRecordController : Controller
